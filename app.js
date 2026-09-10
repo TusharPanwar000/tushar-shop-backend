f9d9e0bc680277b13aea06df2424d0b4892f7069
Tushar Panwar <tusharpanwar0000@gmail.com>
2026-09-10 12:54:16 +0530

    Tushar Shop backend - Signup, Login, JWT authentication

---
+++ app.js
@@ -0,0 +1,77 @@
+require ('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./User')
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(express.json());
+
+
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({username: req.body.username})
+
+  if(!user){
+    return res.status(404).send('User nahi mila');
+  }
+
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+  if(!isMatch){
+    return res.status(404).send('Galat password')
+  }
+
+  const token = jwt.sign(
+    {userId: user._id, username: user.username},
+    process.env.JWT_SECRET, 
+    {expiresIn: '1h'}
+  );
+
+  res.json({massege: 'Login successful!', token: token})
+})
+
+
+function verifyToken(req, res, next){
+  const token = req.headers['authorization'];
+
+  if(!token){
+    return res.status(401).send('Token nahii mila')
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(403).send('token invalid hai');
+        };
+
+        req.user = decoded;
+        next();
+  })
+}
+
+app.get('/profile', verifyToken, (req, res) => {
+  res.json({message: 'Ye protected route hao', user: req.user});
+})
+
+app.post('/signup',async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  const newUser = new User({
+    username: req.body.username,
+    password: hashedPassword
+  });
+
+  await newUser.save();
+  res.status(201).json({message: 'User succesfully done'})
+})
+
+
+
+mongoose.connect(process.env.MONGO_URl)
+.then(() => console.log('Server connect hogya hai'))
+.catch((err) => console.log('Connection error:', err));
+
+
+app.listen(3000, () => {
+  console.log('Server chal raaha hai https://localhost:3000 pr ')
+})
